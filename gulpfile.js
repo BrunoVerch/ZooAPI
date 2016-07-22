9fc588504dee5645b089c6e249c540e295062e4c
BrunoVerch <bruno_verch@hotmail.com>
2016-07-22 00:24:07 -0300

    Retirado dependencias do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,11 @@ var nodemon = require('gulp-nodemon');
 var jshint  = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var complexity  = require('gulp-complexity');
-var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var path = require('path');
 
 var paths = {
     src: ['./src/**/*.js'],
-    build: 'build',
     sourceRoot: path.join(__dirname,'src')
 }
 
@@ -19,7 +17,7 @@ gulp.task('start', function() {
 	nodemon({
 	    script: 'build/server.js',
 	    ext: 'js html',
-	    tasks: ['babel','lint']
+	    tasks: ['lint']
 	})
 });
 
@@ -30,14 +28,4 @@ gulp.task('lint', function() {
     .pipe(complexity({
     	jsLintXML: 'report.xml'
     }));
-});
-
-gulp.task('babel', function() {
-    return gulp.src(paths.src)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(sourcemaps.write('.',{ sourceRoot: paths.sourceRoot }))
-        .pipe(gulp.dest(paths.build));
 });
