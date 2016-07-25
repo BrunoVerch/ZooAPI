2ec4894708a75d64a7b9c870d04be4c33a3f41c5
BrunoVerch <bruno_verch@hotmail.com>
2016-07-25 02:12:15 -0300

    Adding qrcode service

---
+++ src/services/qrcode.js
@@ -0,0 +1,13 @@
+const QR = require('qr-image');
+const fs = require('fs');
+const randomstring = require("randomstring");
+
+module.exports = function saveToImage(data) {
+
+    const filePath = '/public/images/qrcode' + randomstring.generate(8) + '.qr.png'; 
+    const qrpng = qr.image(data, { type: 'png', margin: 1 });
+
+    qrpng.pipe(fs.createWriteStream(appRoot + filePath, { autoClose: true }));
+
+    return filePath;
+}
