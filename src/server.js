5e61fe2d24b245a9a4505e64ebc1ee7c67b165b1
BrunoVerch <bruno_verch@hotmail.com>
2016-07-25 02:11:50 -0300

    Routes to static files

--- src/server.js
+++ src/server.js
@@ -3,12 +3,38 @@
 const Hapi = require('hapi');
 const routes = require('./routes/index');
 const models = require('./models/index');
+const Inert = require('inert');
+const Path = require('path');
+const Vision = require('vision');
+global.appRoot = Path.resolve(__dirname);
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+	connections: {
+		routes: {
+			files: {
+				relativeTo: Path.join(__dirname,'public')
+			}
+		}
+	}
+});
+
+server.register(Vision, () => {});
+
+server.views({
+	engines: {
+		ejs: require('ejs')
+	},
+	relativeTo: __dirname,
+	path: './templates',
+	layout: true,
+	layoutPath: Path.join(__dirname, 'templates/layout')
+});
 
 server.connection({
 	port: 3432
 });
+
+server.register(Inert, () => {});
 
 for (let route of routes) {
 	server.route(route);
