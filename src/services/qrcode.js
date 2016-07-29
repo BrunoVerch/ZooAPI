bf8e0a4a3899880976efb787e7dac5ff50508c43
BrunoVerch <bruno_verch@hotmail.com>
2016-07-28 23:29:48 -0300

    changing path to save qrcode

--- src/services/qrcode.js
+++ src/services/qrcode.js
@@ -4,10 +4,10 @@ const randomstring = require("randomstring");
 
 module.exports = function saveToImage(data) {
 
-    const filePath = '/public/images/qrcode' + randomstring.generate(8) + '.qr.png'; 
+    const filePath = '/images/qrcode/' + randomstring.generate(8) + '.qr.png'; 
     const qrpng = QR.image(data, { type: 'png', margin: 1 });
 
-    qrpng.pipe(fs.createWriteStream(appRoot + filePath, { autoClose: true }));
+    qrpng.pipe(fs.createWriteStream(appRoot + '/public' + filePath, { autoClose: true }));
 
     return filePath;
 }
