b4762f54ea95a2941ff43f8e2c3ad3eb2d3cdb5a
Bruno da Silva Verch <bruno_verch@hotmail.com>
2016-12-14 00:42:08 -0200

    ajuste database

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,13 @@
 'use strict';
 
-if(process.env.NODE_ENV){
-	module.exports = {
-		name: 'ZooApp',
-		username: 'root',
-		password: 'verchverch90',
-		options : {
-			dialect: 'mysql'
-		}
-	};
-}
-
-if(process.env.NODE_ENV == 'production'){
-
-	const urlConnection = process.env.CLEARDB_DATABASE_URL;
-	const groups = urlConnection.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
-	module.exports = {
-		name: groups[4],
-		username: groups[1],
-		password: groups[2],
-		options : {
-			host: groups[3],
-			dialect: 'mysql'
-		}
-	};
-}
+const urlConnection = process.env.CLEARDB_DATABASE_URL;
+const groups = urlConnection.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+module.exports = {
+	name: groups[4],
+	username: groups[1],
+	password: groups[2],
+	options : {
+		host: groups[3],
+		dialect: 'mysql'
+	}
+};
